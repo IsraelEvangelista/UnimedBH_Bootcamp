08c20f74747dc5e20e5f1d25cad7d09779498007
Israel Evangelista <116984176+IsraelEvangelista@users.noreply.github.com>
2022-11-19 15:54:41 -0300

    01 - Manipulação de strings
    Primeira manipulação de strings em Python

---
+++ Manipulação de Strings com Python/string_01.py
@@ -0,0 +1,19 @@
+nome = "IsRaEl"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "   Olá Mundo!       "
+
+print(texto)
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
