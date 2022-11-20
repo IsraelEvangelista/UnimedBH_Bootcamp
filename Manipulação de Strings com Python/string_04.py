2cb1f9828fe476e1a9cc12814ab003d8caffd39e
Israel Evangelista <116984176+IsraelEvangelista@users.noreply.github.com>
2022-11-20 17:30:22 -0300

    04 - Manipulação de strings
    String múltiplas linhas

---
+++ Manipulação de Strings com Python/string_04.py
@@ -0,0 +1,23 @@
+nome = "Israel"
+
+# f-string
+mensasagem = f"""
+   Olá meu nome é {nome},
+Eu estou apredendo Python.
+     Esta mensagem tem diferentes recuos.
+"""
+
+print(mensasagem)
+
+# Strings triplas
+print("""
+     ================== MENU ==================
+     
+     1 - Depositar
+     2 - Sacar
+     0 - Sair
+     
+     ==========================================
+
+                     Obrigado por usar nosso sistema!!!
+     """ )
