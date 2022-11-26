c66489f67119d716f78b907e71cdb612d2e8d7a4
Israel Evangelista <116984176+IsraelEvangelista@users.noreply.github.com>
2022-11-26 15:02:13 -0300

    Hands-on: Herança Simples
    Aprendendo o Conceito de Herança com Python

---
+++ Aprendendo o Conceito de Herança com Python/herança_simples.py
@@ -0,0 +1,38 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+moto = Motocicleta("preta", "asc-1234", 2)
+moto.ligar_motor()
+print(moto)
+
+carro = Carro("branco", "xde-0098", 4)
+carro.ligar_motor()
+print(carro)
+
+caminhao = Caminhao("Roxo", "gfd-8712", 8, True)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
+print(caminhao)
